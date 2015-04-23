c9e57d66dd577866749009853a8f8c0ea0b38d98
Jeff Ching <ching.jeff@gmail.com>
2015-04-23 17:14:23 -0400

    initial commit

---
+++ lib/json_api_client/legacy.rb
@@ -0,0 +1,7 @@
+require "json_api_client/legacy/version"
+
+module JsonApiClient
+  module Legacy
+    # Your code goes here...
+  end
+end
---
+++ lib/json_api_client/legacy/version.rb
@@ -0,0 +1,5 @@
+module JsonApiClient
+  module Legacy
+    VERSION = "0.1.0"
+  end
+end
